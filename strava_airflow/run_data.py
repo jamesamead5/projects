4558aa029df07704e4f161ee9b87263357a0d18c
James Mead <jamesamead5@gmail.com>
2023-01-29 17:43:35 +0000

    Add code to correct duplicate records in JSON file and rewrite incorrectly numbered Strava activities

--- strava_airflow/run_data.py
+++ strava_airflow/run_data.py
@@ -2,6 +2,8 @@ import requests
 import json
 import time
 import datetime
+import re
+import sys
 from airflow import DAG
 from airflow.operators.python import PythonOperator, ShortCircuitOperator
 
@@ -25,6 +27,59 @@ def get_access_token():
 
     with open('/Users/jamesmead/Run Data/credentials.json','w') as fid:
         json.dump(creds,fid)
+
+def clean_file():
+
+    with open('/Users/jamesmead/Run Data/run_data.json','r') as fid:
+        run_data = json.load(fid)
+
+    if len(run_data) == 0:
+        return
+
+    for i in range(len(run_data)):
+        if i >= len(run_data):
+            break
+        else:
+            run_data_comp = run_data[:]
+            run_data_comp.pop(i)
+            if run_data[i] in run_data_comp:
+                run_data.pop(i)
+
+    run_data = sorted(run_data, key = lambda val:datetime.datetime.strptime(val['start_date'], "%Y-%m-%dT%H:%M:%SZ"))
+
+    with open('/Users/jamesmead/Run Data/credentials.json','r') as fid:
+        creds = json.load(fid)
+
+    if creds['status_code'] == 200:
+        headers = {"Authorization": "Bearer {}".format(creds['access_token'])}
+        break_num = 0
+        for i in range(len(run_data)):
+            if break_num >= 25:
+                break
+            if re.search('\d+',run_data[i]['name']) == None:
+                url_updt = 'https://www.strava.com/api/v3/activities/{}'.format(run_data[i]['id'])
+                data_updt = {'name':'FJ - {}'.format(i+1)}
+                r_updt = requests.put(url_updt,headers=headers,data=data_updt)
+                break_num = break_num + 1
+                if r_updt.status_code == 200:
+                    run_data[i]['name'] = data_updt['name']
+                else:
+                    sys.exit(1)
+            elif re.search('\d+',run_data[i]['name']).group() != str(i+1):
+                url_updt = 'https://www.strava.com/api/v3/activities/{}'.format(run_data[i]['id'])
+                data_updt = {'name':re.sub('\d+','{}',run_data[i]['name']).format(i+1)}
+                r_updt = requests.put(url_updt,headers=headers,data=data_updt)
+                break_num = break_num + 1
+                if r_updt.status_code == 200:
+                    run_data[i]['name'] = data_updt['name']
+                else:
+                    sys.exit(1)
+
+    else:
+        sys.exit(1)
+
+    with open('/Users/jamesmead/Run Data/run_data.json','w') as fid:
+        json.dump(run_data,fid)
 
 def trigger_other_tasks(ti):
 
@@ -85,10 +140,11 @@ def gather_detailed_info(ti):
                     'elev_low','description','perceived_exertion','splits_metric','best_efforts','similar_activities']
             run = {}
             for val in cols:
-                if val == 'summary_polyline':
-                    run['polyline'] = req['map'][val]
-                else:
-                    run[val] = req[val]
+                if val in req.keys():
+                    if val == 'summary_polyline':
+                        run['polyline'] = req['map'][val]
+                    else:
+                        run[val] = req[val]
             try:
                 weather_url = "https://api.openweathermap.org/data/3.0/onecall/timemachine"
                 weather_params = {'lat':'56.15372056606176','lon':'-3.8009364730092274',
@@ -103,7 +159,10 @@ def gather_detailed_info(ti):
             for val in weather_cols:
                 run[val] = weather_req['data'][0][val]
             new_run_data.append(run)
-            ti.xcom_push(key='run_info', value=new_run_data)
+            with open('/Users/jamesmead/Run Data/run_data_add.json','w') as fid:
+                fid.write('')
+            with open('/Users/jamesmead/Run Data/run_data_add.json','w') as fid:
+                json.dump(new_run_data,fid)
         else:
             raise Exception('Response unsuccessful')
 
@@ -112,7 +171,8 @@ def append_run_data(ti):
     with open('/Users/jamesmead/Run Data/run_data.json','r') as fid:
         run_data = json.load(fid)
 
-    new_run_data = ti.xcom_pull(key='run_info')
+    with open('/Users/jamesmead/Run Data/run_data_add.json','r') as fid:
+        new_run_data = json.load(fid)
 
     for val in new_run_data:
         if val not in run_data:
@@ -132,10 +192,14 @@ start_date=datetime.datetime(2022, 6, 5), catchup=False)
 
 retrieve_access_token = PythonOperator(task_id='access_token_task', python_callable=get_access_token, dag=dag)
 
+start_clean_up = PythonOperator(task_id='START_clean_data', python_callable=clean_file, dag=dag)
+
 continue_tasks = ShortCircuitOperator(task_id='trigger_future_tasks',python_callable=trigger_other_tasks,dag=dag)
 
 get_data = PythonOperator(task_id='get_detailed_info_task',python_callable=gather_detailed_info, dag=dag)
 
 add_run_data = PythonOperator(task_id='add_run_data_task', python_callable=append_run_data, dag=dag)
 
-retrieve_access_token >> continue_tasks >> get_data >> add_run_data
+end_clean_up = PythonOperator(task_id='END_clean_data', python_callable=clean_file, dag=dag)
+
+retrieve_access_token >> start_clean_up >> continue_tasks >> get_data >> add_run_data >> end_clean_up
